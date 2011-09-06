9b3d2c8ecbb72841f068df8ac9563127e10775a2
Terry Brownell <tbrownell1@gmail.com>
2011-09-05 19:45:34 -0600

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate,   :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate,   :only => [:index,:show,:edit, :update, :destroy]
   before_filter :correct_user,   :only => [:edit, :update]
   before_filter :admin_user,     :only => :destroy
   before_filter :signed_in_user, :only => [:new, :create]
@@ -59,10 +59,6 @@ class UsersController < ApplicationController
   end
 
   private
-  
-    def authenticate
-      deny_access unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
